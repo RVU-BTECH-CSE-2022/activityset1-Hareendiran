1232e984e46414b66fb5d8d5a9c17a24bb689feb
Hareendiran N <110167369+Hareendiran@users.noreply.github.com>
2022-08-01 11:47:09 +0000

    p11original

--- p11original.c
+++ p11original.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+int main()
+{
+  float length,breadth,area;
+  printf("enter length and breadth values\n");
+  scanf("%f%f",&length,&breadth);
+  area=length*breadth;
+printf("the area of the rectangle with length=&f and breadth=&f is=%f\n",length,breadth,area);
+  return 0;
+}
