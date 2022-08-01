932b054e2b7b8ab0cd5df1041ccb6394649cdcca
Hareendiran N <110167369+Hareendiran@users.noreply.github.com>
2022-08-01 14:10:17 +0000

    p0original

--- p0original.c
+++ p0original.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main()
 {
-  float length = 10.0;
-  float breadth = 20.0;
-  float area = length * breadth;
-  printf("The area of the rectangle with length = %f and breadth %f is %f\n",length, breadth, area);
+  float base = 10.0;
+  float altitude = 20.0;
+  float area = 0.5 * base * altitude;
+  printf("The area of the triangle with base = %f and altitude %f is %f\n",base, 
+  altitude,area);
   return 0;
 }
