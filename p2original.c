80cef85b9f00e91d22a4c42116d926496ac29ba9
Hareendiran N <110167369+Hareendiran@users.noreply.github.com>
2022-08-04 11:47:08 +0000

    largest in three numbers

---
+++ p2original.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int input()
+{
+  int base;
+  printf("enter base value");
+  scanf("%d",&base);
+  return base;
+
+  int altitude;
+  printf("enter altitude value");
+  scanf("%d",&altitude);
+  return altitude;
+}
