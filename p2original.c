3b99168d7799071c5cccd22b1fb36f2f872410a5
Hareendiran N <110167369+Hareendiran@users.noreply.github.com>
2022-08-04 14:29:06 +0000

    triangle area using function

--- p2original.c
+++ p2original.c
@@ -4,14 +4,14 @@ float find_area(float base, float altitude)
   return (base*altitude/2);
 }
 
-int input()
+int main()
 {
   float base,altitude,area;
   printf("enter base value");
-  scanf("%d",&base);
+  scanf("%f",&base);
 
   printf("enter altitude value");
-  scanf("%d",&altitude);
+  scanf("%f",&altitude);
 
   area=find_area(base,altitude);
   printf("area is %f\n",area);
